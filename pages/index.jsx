4128f3321c312f7d58a393233ec2a461a4537f47
GokulDev4U <sreeharipisharody678@gmail.com>
2022-01-10 12:59:10 +0530

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Feed from "../components/Feed";
+import Sidebar from "../components/Sidebar";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className='bg-black min-h-screen flex max-[1500px] mx-auto'>
+        <Sidebar />
+        <Feed />
+        {/* Widgets */}
+
+        {/* Modal */}
+      </main>
+
+    </div>
+  );
+}
